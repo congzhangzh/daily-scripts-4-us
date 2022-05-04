1463a9de34935eaef253c62c3bcad7c0df26e0c7
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2022-05-04 09:42:14 +0800

    Update find_your_pi.sh
    improve by idea of https://stackoverflow.com/questions/11097761/is-there-a-way-to-make-bash-job-control-quiet

--- find_your_pi.sh
+++ find_your_pi.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-
+# refs:
+# 1. https://stackoverflow.com/questions/11097761/is-there-a-way-to-make-bash-job-control-quiet
 #--begin-- input region
 network=192.168.1
 #--end-- inut region
 
+# one: for run in shell script 
 for i in {2..254} ; do echo "192.168.1.$i: `nc -w 2 192.168.1.$i  22`" & done
+
+# two: for copy and paste to interactive shell
+#set -m
+#for i in {2..254} ; do echo "192.168.1.$i: `nc -w 2 192.168.1.$i  22`" & done
+#set +m
 
 # then check you console, open ssh server will response something
