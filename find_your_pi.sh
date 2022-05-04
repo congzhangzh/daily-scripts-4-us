936b5615e9212aadb91c060003c113d5f19e10fd
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2022-05-04 10:02:40 +0800

    Update find_your_pi.sh

--- find_your_pi.sh
+++ find_your_pi.sh
@@ -6,11 +6,6 @@ network=192.168.1
 #--end-- inut region
 
 # one: for run in shell script 
-for i in {2..254} ; do echo "192.168.1.$i: `nc -w 2 192.168.1.$i  22`" & done
-
-# two: for copy and paste to interactive shell
-#set -m
-#for i in {2..254} ; do echo "192.168.1.$i: `nc -w 2 192.168.1.$i  22`" & done
-#set +m
+for i in {2..254} ; do ( echo "192.168.1.$i: `nc -w 2 192.168.1.$i  22`" & ) done
 
 # then check you console, open ssh server will response something
