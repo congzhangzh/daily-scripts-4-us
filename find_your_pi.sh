644a3d50d529bdd171ffa2777a9ebb5251ca88cd
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2022-05-04 09:31:58 +0800

    Update find_your_pi.sh
    use a fast parallel scan way

--- find_your_pi.sh
+++ find_your_pi.sh
@@ -4,6 +4,6 @@
 network=192.168.1
 #--end-- inut region
 
-for i in {2..254} ; do echo $network.$i ; nc -w 2 $network.$i ; done
+for i in {2..254} ; do echo "192.168.1.$i: `nc -w 2 192.168.1.$i  22`" & done
 
 # then check you console, open ssh server will response something
