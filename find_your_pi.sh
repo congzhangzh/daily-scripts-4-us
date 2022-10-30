4df3a85f55cfa29f980b06c91e3061ea23cc011a
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2022-10-30 10:16:44 +0800

    Update find_your_pi.sh

--- find_your_pi.sh
+++ find_your_pi.sh
@@ -2,10 +2,10 @@
 # refs:
 # 1. https://stackoverflow.com/questions/11097761/is-there-a-way-to-make-bash-job-control-quiet
 #--begin-- input region
-network=192.168.1
+network=192.168.71
 #--end-- inut region
 
 # one: for run in shell script 
-for i in {2..254} ; do ( echo "192.168.1.$i: `nc -w 2 192.168.1.$i  22`" & ) done
+for i in {2..254} ; do ( echo "$network.$i: `nc -w 2 $network.$i  22`" & ) done
 
 # then check you console, open ssh server will response something
